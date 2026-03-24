da4a9d25d8d6c5119557a652b8f015aef9021f6d
Crim3z <86653237+Crim3z@users.noreply.github.com>
2026-03-24 11:25:21 +0800

    Fix typos and improve pyramid number generation logic

--- Act7_pyramid_number.java
+++ Act7_pyramid_number.java
@@ -1,5 +1,5 @@
 /**
- * Desiagn a program that will generatea pyramidfor a gevin number
+ * Design a program that will generate a pyramid for a given number
  * using for loop statement
  *
  * pangalan mo!!!!!
@@ -12,19 +12,17 @@ public class Act7_pyramid_number
         Scanner input = new Scanner(System.in);
         System.out.print("PYRAMID OF NUMBERS");
         System.out.println();
-        System.out.print("Enter First Number :");
+        System.out.print("Enter First Number: ");
         int a = input.nextInt();
-        System.out.print("Enter Second Number :");
+        System.out.print("Enter Second Number: ");
         int b = input.nextInt();
-
-        for(int i=1;i<=a;i++)
+        for(int i = a; i <= b; i++)
         {
-            for (int j=b; j <= i; j++)
+            for(int j = 1; j <= i; j++)
             {
-                System.out.print("" +i+ "");
+                System.out.print(i + " ");
             }
             System.out.println();
         }
-
     }
 }//End
